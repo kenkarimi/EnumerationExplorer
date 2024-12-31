b0e9bfca5dc9449cad2727757bd7e369b7cd9075
Kennedy Karimi on Mac <kennedymugera@gmail.com>
2024-12-31 14:20:14 +0300

    Initial Commit

---
+++ EnumerationExplorer/EnumerationExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnumerationExplorerApp.swift
+//  EnumerationExplorer
+//
+//  Created by Kennedy Karimi on 31/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct EnumerationExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnumerationExplorerTests/EnumerationExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnumerationExplorerTests.swift
+//  EnumerationExplorerTests
+//
+//  Created by Kennedy Karimi on 31/12/2024.
+//
+
+import Testing
+@testable import EnumerationExplorer
+
+struct EnumerationExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
